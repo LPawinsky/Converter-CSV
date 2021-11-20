71f30f341debf2863de76fc497bed50bb709490e
Luekk <74063156+Luekk@users.noreply.github.com>
2021-11-20 13:39:03 +0100

    labels edit

--- main.py
+++ main.py
@@ -18,14 +18,20 @@ def error_label(error: str) -> str:
     label.pack()
     label.place(relx=0.5,rely=0.2,anchor=CENTER)
 
-def path_label(path):
-    print(path)
+def path_label(path: str) -> str:
+    label = Label(root, text=path)
+    label.pack()
+    label.place(relx=0.2,rely=0.33,anchor=CENTER)
 
-def output_label(path):
-    print(path)
+def output_label(path: str) -> str:
+    label = Label(root, text=path)
+    label.pack()
+    label.place(relx=0.6,rely=0.33,anchor=CENTER)
 
-def filename_label(filename):
-    print(filename)
+def filename_label(filename: str) -> str:
+    label = Label(root, text=filename)
+    label.pack()
+    label.place(relx=0.5,rely=0.3,anchor=CENTER)
 
 def exit():
     sys.exit(root)
@@ -35,10 +41,13 @@ def choose_file_path():
     state.path = file_path
     head, tail = os.path.split(state.path)
     state.filename = tail
+    path_label(state.path)
+    filename_label(state.filename)
 
 def get_output():
     output_path = filedialog.askdirectory()
     state.output = output_path
+    output_label(state.output)
 
 def start_conversion():
     try:
@@ -53,7 +62,7 @@ def start_conversion():
                 error_label("Brak atrybutów")
 
 root = Tk()
-root.title('Konwerter kwartałów')
+root.title(f'Konwerter kwartałów v{state.version}')
 root.geometry('600x400')
 
 button = Button(root, text="Wybierz plik", command=choose_file_path)
