2a0c55d8b67d46d8f6f0c1ed303f04d7b8babbd5
Luekk <74063156+Luekk@users.noreply.github.com>
2021-12-01 12:33:42 +0100

    v0.2 test

---
+++ daily_script.py
@@ -0,0 +1,48 @@
+import pandas as pd
+from txt_convert import txt_convert
+
+def columns_add(data):
+    if 'Vol' not in sorted(data):
+        data['Vol'] = 0
+    if 'Vol' in sorted(data):
+        data['OpenInt'] = 0
+        return data
+
+def indices_of_dates(df,third_fridays):
+    indices = []
+    for date in df['Date']:
+        for friday in third_fridays:
+            if date == friday.strftime("%Y-%m-%d"):
+                index = df.index[df['Date'] == friday.strftime("%Y-%m-%d")]
+                indices.append(index)
+    return indices
+
+def add_open_int(df):
+    third_fridays = pd.date_range(df['Date'].iloc[len(df.index)-len(df.index)], df['Date'].iloc[len(df.index)-1], freq='WOM-3FRI')
+    indices = indices_of_dates(df,third_fridays)
+    for i in indices:
+        date = df['Date'].iloc[i]
+        month = pd.DatetimeIndex(date).month_name()
+        if month[0] == 'March' or month[0] == 'June' or month[0] == 'September' or month[0] == 'December':
+            df.at[i, 'OpenInt'] = 3
+        else:
+            df.at[i, 'OpenInt'] = 1
+    return df
+
+def english_check(data):
+    if sorted(data)[0] == 'Data':
+        df = pd.DataFrame(data, columns=['Data','Najwyzszy', 'Najnizszy','Otwarcie','Zamkniecie'])
+        df = df.rename({'Najwyzszy':'High', 'Najnizszy':'Low', 'Otwarcie':'Open', 'Zamkniecie':'Close', 'Data':'Date',}, axis='columns')
+        return df
+    if sorted(data)[0] != 'Data':
+        return data
+
+def daily_script(path, output):
+    data = pd.read_csv(path)
+    df = pd.DataFrame(data)
+
+    with_all_columns = columns_add(english_check(df))
+    correct_dataframe_with_periods = add_open_int(with_all_columns)
+    txt_convert(correct_dataframe_with_periods, path, output)
+
+# daily_script('/Users/marianpazdzioch/Desktop/program/eurusd_d.csv', '/Users/marianpazdzioch/Desktop/program')
---
+++ quarter_script.py
@@ -0,0 +1,110 @@
+import pandas as pd
+import datetime as dt
+from txt_convert import txt_convert
+
+data_to_df = []
+
+def create_quarters(df):
+    df.date = pd.to_datetime(df.Date)
+    df['Quarter'] = pd.PeriodIndex(df.date, freq='Q')
+    return df
+
+def create_names_from_date(df):
+    df.date = pd.to_datetime(df.Date)
+    df['Dayname'] = df.date.dt.day_name()
+    df['Day'] = pd.DatetimeIndex(df['Date']).day
+    df['Month'] = df.date.dt.month_name().str[:3]
+    df['Year'] = pd.DatetimeIndex(df['Date']).year
+    return df
+
+def period_create(df):
+    periods = []
+    third_fridays = pd.date_range(df['Date'].iloc[len(df.index)-len(df.index)], df['Date'].iloc[len(df.index)-1], freq='WOM-3FRI')
+    starting_days_array = []
+    ending_days_array = []
+    for date in third_fridays:
+        date = date + dt.timedelta(days=3)
+        if date.month == 3:
+            starting_days_array.append(date)
+        if date.month == 6:
+            starting_days_array.append(date)
+        if date.month == 9:
+            starting_days_array.append(date)
+        if date.month == 12:
+            starting_days_array.append(date)
+    for date in third_fridays:
+        if date.month == 3:
+            ending_days_array.append(date)
+        if date.month == 6:
+            ending_days_array.append(date)
+        if date.month == 9:
+            ending_days_array.append(date)
+        if date.month == 12:
+            ending_days_array.append(date)
+    if len(starting_days_array) == len(ending_days_array):
+        for index, date in enumerate(starting_days_array):
+            ending_days_array_index = len(ending_days_array)-1
+            if index < ending_days_array_index:
+                periods.append(date)
+                periods.append(ending_days_array[index+1])
+            if index == ending_days_array_index:
+                periods.append(date)
+    return periods
+
+def get_prices_of_period(df, startDay, endDay):
+    after_start_date = df["Date"] >= startDay
+    before_end_date = df["Date"] <= endDay
+    between_two_dates = after_start_date & before_end_date
+    filtered_dates = df.loc[between_two_dates]
+    highest_price = filtered_dates['High'].max()
+    lowest_price = filtered_dates['Low'].min()
+    opening_price = filtered_dates["Open"].iloc[len(filtered_dates.index)-len(filtered_dates.index)]
+    closing_price = filtered_dates['Close'].iloc[len(filtered_dates.index)-1]
+    return startDay, endDay, highest_price, lowest_price, opening_price, closing_price
+
+def write_single_period(data):
+    data_to_df.append([data[1], data[4], data[2], data[3], data[5], 0, 0])
+
+def create_formatted_df(data, path, output_path):
+    df = pd.DataFrame(data, columns = ['Date', 'Open', 'High', 'Low', 'Close','Vol','OpenInt'])
+    print(df)
+    txt_convert(df, path, output_path)
+
+def quarter_script(path, output):
+    data = pd.read_csv(path)
+    df = pd.DataFrame(data, columns=['Data','Najwyzszy', 'Najnizszy','Otwarcie','Zamkniecie','Vol','OpenInt'])
+    df = df.rename({'Najwyzszy':'High', 'Najnizszy':'Low', 'Otwarcie':'Open', 'Zamkniecie':'Close', 'Data':'Date',}, axis='columns')
+    print(data)
+    quarterDataFrame = create_quarters(df)
+    full_data_frame = create_names_from_date(quarterDataFrame)
+    periods = period_create(full_data_frame)
+    for i in range(0, len(periods), 2):
+        if i < len(periods)-1:
+            normal_time_start = periods[i].to_datetime64()
+            normal_time_end = periods[i+1].to_datetime64()
+            s = pd.to_datetime(normal_time_start)
+            s = s.date()
+            e = pd.to_datetime(normal_time_end)
+            e = e.date()
+            data = get_prices_of_period(full_data_frame, s.strftime("%Y-%m-%d"), e.strftime("%Y-%m-%d"))
+            write_single_period(data)
+        if i == len(periods)-1:
+            last_index = df['Date'].index[-2]
+            normal_time_start = periods[i].to_datetime64()
+            normal_time_end = df['Date'][last_index]
+            s = pd.to_datetime(normal_time_start)
+            s = s.date()
+            e = pd.to_datetime(normal_time_end)
+            e = e.date()
+            data = get_prices_of_period(full_data_frame, s.strftime("%Y-%m-%d"), e.strftime("%Y-%m-%d"))
+            write_single_period(data)
+        if len(periods) / 2 % 1:
+            length = len(periods) / 2 - 0.5
+            if len(data_to_df) == int(length):
+                create_formatted_df(data_to_df, path, output)
+        if len(periods) / 2 == len(data_to_df):
+            print("true")
+        formatted_periods = len(periods) / 2
+
+# quarter_script('/Users/marianpazdzioch/Desktop/program/eurusd_d.csv', '/Users/marianpazdzioch/Desktop/program')
+    
